8da9b86589362f156fe50376972bd69db04dfa5a
heitordeve <heitor.developer@outlook.com>
2024-10-07 10:41:21 -0300

    [FEAT] - Created route status API

--- laravel_api_with_sanctum/routes/api.php
+++ laravel_api_with_sanctum/routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('status', function(){
+    return response()->json(
+        [
+            'status' => 'ok',
+            'message' => 'API is Running'
+        ]
+        );
 });
