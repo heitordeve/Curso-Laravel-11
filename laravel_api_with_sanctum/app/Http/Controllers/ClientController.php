f0825200d60a2d183dd42f04bc5d2c4e1d5776f0
heitordeve <heitor.developer@outlook.com>
2024-10-07 11:16:36 -0300

    >>[FEAT} - Created endpoint List all Clients<<
    1 - Created method INDEX for list all Clients
    
    Signed-off-by: heitordeve <heitor.developer@outlook.com>

--- laravel_api_with_sanctum/app/Http/Controllers/ClientController.php
+++ laravel_api_with_sanctum/app/Http/Controllers/ClientController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
 use Illuminate\Http\Request;
 
 class ClientController extends Controller
@@ -11,7 +12,7 @@ class ClientController extends Controller
      */
     public function index()
     {
-        //
+        return response()->json(Client::all(), 200);
     }
 
     /**
